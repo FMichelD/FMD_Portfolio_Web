eb1e5fb4b24aec02372200d810e139d1bbfeaefa
FMichelD <fmicheld@gmail.com>
2022-08-24 19:36:49 -0300

    remove diretorios 'duplicados'

--- Pokedex/JS/toggle_menu.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById('menu-button')
-
-button.addEventListener('click', function(event){
-    event.preventDefault()
-    let menu = document.getElementById('pokemon-menu')
-    menu.classList.toggle('is-open')
-})
